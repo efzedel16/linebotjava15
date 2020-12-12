748a0002770490bff9134d0d2abb7e97c4092442
efzedel16 <efzedel16@outlook.com>
2020-12-12 17:18:47 +0700

    initial commit

---
+++ src/main/java/com/dicoding/ImplementasiFiturJava15/ImplementasiFiturJava15Application.java
@@ -0,0 +1,13 @@
+package com.dicoding.ImplementasiFiturJava15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementasiFiturJava15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementasiFiturJava15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicoding/ImplementasiFiturJava15/ImplementasiFiturJava15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.ImplementasiFiturJava15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementasiFiturJava15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
